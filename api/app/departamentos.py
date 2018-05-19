d39d0091bfe048b8e1771916f02444e96eb3c377
Radeori <radeori@gmail.com>
2018-05-19 15:03:10 -0300

    Implementada leitura de arquivos de scores (servico 3) e algum tratamento de excecao

--- api/app/departamentos.py
+++ api/app/departamentos.py
@@ -7,18 +7,44 @@ from flask import request
 # 3. Scores de todos os departamentos em uma área
 # 4. Score de um determinado departamento em uma área.
 
-@app.route('/score')
+@app.route("/score")
 def obterScoresAreaPorDepartamento():
-	area = request.args.get('area')
-	departamento = request.args.get('departamento')
 	retorno = "<h1>"
-	if area == None:
-		retorno += "Especifique na url /score?area=<area>&departamento=<departamento>(opcional)"
-	else:
-		retorno += "Obtendo scores para a área ".decode('utf8') + area
-		if departamento == None:
-			retorno += " de todos seus departamentos"
+	try:
+		area = request.args.get("area")
+		departamento = request.args.get("departamento")
+		if area == None:
+			retorno += "Especifique na url /score?area=<area>&departamento=<departamento>(opcional)"
 		else:
-			retorno += " e departamento " + departamento
-	retorno += "...</h1>"
-	return retorno
+			retorno += "Obtendo scores para a área ".decode("utf8") + area
+			if not departamento:
+				retorno += " de todos seus departamentos"
+			else:
+				retorno += " e departamento " + departamento
+	except:
+		app.logger.exception("Exceção disparada na leitura da URL!")
+		raise
+	else:
+		app.logger.debug("Leitura da URL foi um sucesso!")
+	finally:
+		retorno += "...</h1>"
+		try:
+			areaParaBusca = area.lower()
+			arquivo = open("..\\data\\" + areaParaBusca + "-out-scores.csv")
+			retorno += "<h2>"
+			if not departamento:
+				retorno += arquivo.read().replace("\n","<br />")
+			else:
+				retorno += "Em construção".decode("utf8")
+			retorno += "</h2>"
+		except FileNotFoundError:
+			retorno += "<h1>Scores não encontrados para a área ".decode("utf8") + area + "</h1>"
+			app.logger.exception("Exceção disparada na leitura dos arquivos!")
+			raise
+		except:
+			app.logger.exception("Exceção disparada na leitura dos arquivos!")
+			raise
+		else:
+			app.logger.debug("Leitura dos arquivos foi um sucesso!")
+		finally:
+			return retorno
