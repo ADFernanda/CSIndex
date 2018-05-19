369c9f47d39a3fbf9d9c35f09a892afbfb8b932f
Fernanda <fernanda.aduarte@hotmail.com>
2018-05-19 15:51:17 -0300

    Serviço 1 implementado ainda sem tratamentos de erro

--- api/app/conferencias.py
+++ api/app/conferencias.py
@@ -1,18 +1,25 @@
 from app import app
 from flask import request
 import csv
+import numpy
 
 # servicos 1 e 2 
 
+
 @app.route('/obterNumeroPublicacoes')
 def obterNumeroPublicacoes():
-    area = request.args.get('conferencia')
+    conferencia = request.args.get('conferencia')
     area = request.args.get('area')
 
     path = '../data/'
     nomeArquivo = path + area + '-out-papers.csv'
     
-    with open(nomeArquivo) as csvfile:
-        papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    numPublicacoes = 0
 
-    return 'oi'
+    with open(nomeArquivo, 'rb') as csvfile:
+        papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        for row in papers:
+            if (row[1] == conferencia):
+                numPublicacoes = numPublicacoes + 1
+    
+    return str(numPublicacoes)
