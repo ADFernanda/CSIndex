4e6af956ec9cceaab51df325a4c9edb99257a373
Radeori <radeori@gmail.com>
2018-05-19 17:35:41 -0300

    Ultimas alteracoes em departamentos para .csv, antes de refactor

--- api/app/departamentos.py
+++ api/app/departamentos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from app import app
-from flask import request
+from flask import request, make_response
 from os import path
 
 # servicos 3 e 4
@@ -37,13 +37,18 @@ def obterScoresAreaPorDepartamento():
 			arquivo = open(path.join("..","data",areaParaBusca) + "-out-scores.csv")
 			retorno += "<h2>"
 			if not departamento:
-				retorno += arquivo.read().replace("\n","<br />")
+				saida = arquivo.read()
+				retorno = saida.replace("\n","<br />")
+				saida = make_response(saida)
 			else:
-				scoreUnico = filter(lambda linha: linha.find(departamento+",") == 0, arquivo)
-				if not scoreUnico:
+				saida = filter(lambda linha: linha.find(departamento+",") == 0, arquivo)
+				if not saida:
 					retorno += "Score não encontrado para o departamento ".decode("utf8") + departamento
 				else:
-					retorno += scoreUnico[0]
+					retorno += saida[0]
+					saida = make_response(saida[0])
+			if 'saida' in locals():
+				saida = (saida, 200, {"Content-Disposition": 'inline; filename="queryScores.csv"', "Content-Type": "text/csv"})
 			retorno += "</h2>"
 		except IOError:
 			retorno += "<h1>Scores não encontrados para a área ".decode("utf8") + area + "</h1>"
@@ -55,4 +60,7 @@ def obterScoresAreaPorDepartamento():
 		else:
 			app.logger.debug("Leitura dos arquivos foi um sucesso!")
 		finally:
-			return retorno
+			if 'saida' in locals():
+				return saida
+			else:
+				return retorno
