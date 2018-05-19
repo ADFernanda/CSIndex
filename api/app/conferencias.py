80b44a1da7082f2da6f1899b32a7675c13e98e66
Fernanda <fernanda.aduarte@hotmail.com>
2018-05-19 15:05:55 -0300

    Leitura de arquivo em um array

--- api/app/conferencias.py
+++ api/app/conferencias.py
@@ -1,7 +1,18 @@
 from app import app
-
-@app.route('/conferencias/areas/bterNumeroPublicacoes')
-def obterNumeroPublicacoes():
-    return "teste"
+from flask import request
+import csv
 
 # servicos 1 e 2 
+
+@app.route('/obterNumeroPublicacoes')
+def obterNumeroPublicacoes():
+    area = request.args.get('conferencia')
+    area = request.args.get('area')
+
+    path = '../data/'
+    nomeArquivo = path + area + '-out-papers.csv'
+    
+    with open(nomeArquivo) as csvfile:
+        papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    return 'oi'
