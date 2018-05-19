cb91ce7b5aaafc5763b3f1ef9a53aaa3552970c5
Fernanda <fernanda.aduarte@hotmail.com>
2018-05-19 16:33:13 -0300

    Tratamentos de erros

--- api/app/conferencias.py
+++ api/app/conferencias.py
@@ -4,11 +4,9 @@ from flask import Response
 import csv
 from os import path
 
-# servicos 1 e 2 
-
-
-@app.route('/obterNumeroPublicacoes')
-def obterNumeroPublicacoes():
+# servico 1: Numero de publicacoes no conjunto de conferencias de uma area
+@app.route('/obterNumeroPublicacoesEmConferenciaEArea')
+def obterNumeroPublicacoesEmConferenciaEArea():
     conferencia = request.args.get('conferencia')
     area = request.args.get('area')
 
@@ -20,15 +18,44 @@ def obterNumeroPublicacoes():
     try:
         nomeArquivo = path.join("..","data", area) + "-out-papers.csv"        
 
-        with open(nomeArquivo, 'rb') as csvfile:
-            papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-            for row in papers:
-                if (row[1] == conferencia):
-                    numPublicacoes += 1
-    except FileNotFoundError:
-        return Response(status=404)
-        raise
+        try:
+            with open(nomeArquivo, 'rb') as csvfile:
+                papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+                for row in papers:
+                    if (row[1] == conferencia):
+                        numPublicacoes += 1
+        except FileNotFoundError:
+            return Response(status=404)
+            raise
+
     except:
+        return Response(status=500)
+        raise
+    finally:
+        return str(numPublicacoes)
+
+# servico 2: Numero de publicacoes no conjunto de conferencias de uma area
+@app.route('/obterNumeroPublicacoesEmArea')
+def obterNumeroPublicacoesEmArea():
+    area = request.args.get('area')
+
+    if not area:
+        return Response(status=400)
+
+    numPublicacoes = 0
+
+    try:
+        nomeArquivo = path.join("..","data", area) + "-out-papers.csv"        
+
+        try:
+            with open(nomeArquivo, 'rb') as csvfile:
+                papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+                for row in papers:
+                    numPublicacoes += 1
+        except FileNotFoundError:
+            return Response(status=404)
+            raise
+    except Exception:
         return Response(status=500)
         raise
     finally:
