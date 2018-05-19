ba3a0c3d77dad8d6fde5df6032ddc9d38a88cca2
Fernanda <fernanda.aduarte@hotmail.com>
2018-05-19 19:16:09 -0300

    Serviçoes 7,8,9 e 10 finalizados

--- api/app/papers.py
+++ api/app/papers.py
@@ -1,11 +1,92 @@
 from app import app
-from flask import request
+from flask import request, make_response
 from flask import Response
 import csv
 from os import path
 
-# 8. Todos os papers de uma area em um determinado ano
-# 9. Todos os papers de um departamento em uma area
-# 10. Todos os papers de um professor (dado o seu nome)
-
 # servico 7. Todos os papers de uma area (ano, titulo, deptos e autores)
+@app.route('/obterPapersEmArea')
+def obterPapersEmArea():
+    area = request.args.get('area')
+
+    if not area:
+        return Response(status=400)
+
+    try:
+        nomeArquivo = path.join("..","data", area) + "-out-papers.csv"        
+        csvfile =  open(nomeArquivo)
+        saida = csvfile.read()
+    except IOError:
+        return Response(status=404)        
+    except:
+        return Response(status=500)
+    return (make_response(saida), 200, {"Content-Disposition": 'inline; filename="obterPapersEmArea.csv"', "Content-Type": "text/csv"})
+
+# 8. Todos os papers de uma area em um determinado ano
+@app.route('/obterPapersEmAreaEmAno')
+def obterPapersEmAreaEmAno():
+    area = request.args.get('area')
+    ano = request.args.get('ano')
+
+    if not area or not ano:
+        return Response(status=400)
+
+    try:
+        retorno=''
+        nomeArquivo = path.join("..","data", area) + "-out-papers.csv"   
+        with open(nomeArquivo, 'rb') as csvfile:
+            papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            for row in papers:
+                if (row[0] == ano):
+                    line = str(row)
+                    line = line.replace("'","")
+                    retorno += str(line[1:-1]) + '\n'
+    except IOError:
+        return Response(status=404)        
+    except:
+        return Response(status=500)
+    return (make_response(retorno), 200, {"Content-Disposition": 'inline; filename="obterPapersEmAreaEmAno.csv"', "Content-Type": "text/csv"})
+
+# 9. Todos os papers de um departamento em uma area
+@app.route('/obterPapersEmAreaEmDepartamento')
+def obterPapersEmAreaEmDepartamento():
+    area = request.args.get('area')
+    departamento = request.args.get('departamento')
+
+    if not area or not departamento:
+        return Response(status=400)
+
+    try:
+        retorno=''
+        nomeArquivo = path.join("..","data", area) + "-out-papers.csv"   
+        with open(nomeArquivo, 'rb') as csvfile:
+            papers = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            for row in papers:
+                if (row[3] == departamento):
+                    line = str(row)
+                    line = line.replace("'","")
+                    retorno += str(line[1:-1]) + '\n'
+    except IOError:
+        return Response(status=404)        
+    except:
+        return Response(status=500)
+    return (make_response(retorno), 200, {"Content-Disposition": 'inline; filename="obterPapersEmAreaEmAno.csv"', "Content-Type": "text/csv"})
+
+# 10. Todos os papers de um professor (dado o seu nome)
+@app.route('/obterPapersDeProfessor')
+def obterPapersDeProfessor():
+    professor = request.args.get('professor')
+
+    if not professor:
+        return Response(status=400)
+
+    try:
+        retorno=''
+        nomeArquivo = path.join("..","data","profs", "search", professor) + ".csv"   
+        csvfile =  open(nomeArquivo)
+        saida = csvfile.read()
+    except IOError:
+        return Response(status=404)        
+    except:
+        return Response(status=500)
+    return (make_response(saida), 200, {"Content-Disposition": 'inline; filename="obterPapersDeProfessor.csv"', "Content-Type": "text/csv"})
