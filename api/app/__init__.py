3bb963493445f330c701246c15b9b1157c65cc0b
Fernanda <fernanda.aduarte@hotmail.com>
2018-05-20 12:16:05 -0300

    Iniciado cliente

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.after_request
+def enable_cors_all_origins(response):
+	response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin') or '*'
+	response.headers['Access-Control-Allow-Credentials'] = 'true'
+	response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+	return response
 
 from app import conferencias
 from app import papers
